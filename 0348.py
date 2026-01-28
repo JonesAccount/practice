d883039e1a2ce6c7d9ea4525bc0f7f8887dc4ec3
Jones. <jonesmailhub@gmail.com>
2026-01-28 22:41:48 +0500

    Create 0348.py

---
+++ 0348.py
@@ -0,0 +1,10 @@
+words = {"SoLolEarn", "PytHoN", "cOde", "liST"}
+
+cap_list = [i.capitalize() for i in words]
+print(cap_list)
+
+up_list = [i.upper() for i in words]
+print(up_list)
+
+low_list = [i.lower() for i in words]
+print(low_list)
