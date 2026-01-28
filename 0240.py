94085b89bce8a78ce1de82622fe5ff02b14e76d7
Jones. <jonesmailhub@gmail.com>
2026-01-28 20:51:15 +0500

    Create 0240.py

---
+++ 0240.py
@@ -0,0 +1,10 @@
+word1 = "cow"
+print(word1.upper())
+
+word2 = "airplane"
+print(word2.upper())
+
+word3 = "group"
+print(word3.upper())
+
+# Метод upper() возращает копию строки, в которой все буквы сконвертированы к большому регистру (заглавные буквы).
