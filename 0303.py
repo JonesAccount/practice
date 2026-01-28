06660ea37f834b224f180ce950c440b0d8ea08ad
Jones. <jonesmailhub@gmail.com>
2026-01-28 22:19:54 +0500

    Create 0303.py

---
+++ 0303.py
@@ -0,0 +1,9 @@
+word = "python"
+nums = 12, 67, 4, 100
+booling = True, None, False
+nums_float = 67.4, 10.0, 5.5
+
+print(tuple(word))
+print((nums))
+print(tuple(booling))
+print(tuple(nums_float))
