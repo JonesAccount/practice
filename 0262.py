5417177a7dbe27e9b55516036daaccbf5e6717f1
Jones. <jonesmailhub@gmail.com>
2026-01-28 21:01:29 +0500

    Create 0262.py

---
+++ 0262.py
@@ -0,0 +1,15 @@
+def function1():
+    print("First function")
+
+def function2(word):
+    print(word + ")")
+
+def function3(a, b):
+    c = a + b
+    print("Результат: " + str(c))
+
+function1()
+
+function2("Second function")
+
+function3(15, 20)
