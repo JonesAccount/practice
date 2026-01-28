d82ed6fdeb6125ea88173fa76f743fc44b1ca36b
Jones. <jonesmailhub@gmail.com>
2026-01-28 21:56:22 +0500

    Create 0275.py

---
+++ 0275.py
@@ -0,0 +1,8 @@
+def greetings(name, language):
+    print("Здравствуй, " + name + "!")
+    print("Ты учишь " + language + "😊)")
+
+qu_name = input()
+qu_language = input()
+
+greetings(qu_name, qu_language)
