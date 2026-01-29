7fa995f25e7d4ef7cfe39927eda6b14969dd860f
Jones. <jonesmailhub@gmail.com>
2026-01-29 09:31:52 +0500

    Create 0434.py

---
+++ 0434.py
@@ -0,0 +1,8 @@
+number = int(input("Введи число от 1 до 3: "))
+
+if number == 2:
+    print("\nПобеда! Угадал!")
+elif number == 1:
+    print("\nНемного не верно :(")
+else:
+    print("\nПовезет в следущий раз :(")
