7209514dca7b69e6313b47d5b2a19b94e056018b
Jones. <jonesmailhub@gmail.com>
2026-01-29 07:47:48 +0500

    Create 0399.py

---
+++ 0399.py
@@ -0,0 +1,17 @@
+print("Текущее давление:")
+
+pressure = "- 150"
+print(pressure)
+
+print("\nДавление снижается:")
+
+pressure = "- 140"
+print(pressure)
+
+pressure = "- 130"
+print(pressure)
+
+print("\nНорма:")
+
+pressure = "- 120"
+print(pressure)
