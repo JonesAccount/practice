6dcf6705c4a6e9ae163be578be2fb7d063314862
Jones. <jonesmailhub@gmail.com>
2026-01-29 09:59:55 +0500

    Create 0461.py

---
+++ 0461.py
@@ -0,0 +1,72 @@
+# Переменые
+play_game = False
+line = "-" * 30
+dot = "•   •   •   •   •   •   •   •"
+action_home = None
+action_tree = None
+tree = 0
+coin = 0
+
+print("• ИГРА ДРОВОСЕК •", end="\n\n"); print(line)
+
+while True:
+    
+    if play_game == True:
+        print("\n", line, sep="")
+        for i in range(5):
+            print(dot)
+        print(line)
+    play_game = True
+    
+    print("|МЕНЮ|\t\t\tid5725")
+    print("• Продать дерево")
+    print("• Улучшить топор")
+    print("• Лес", end="\n\n")
+    
+    print("|ИНВЕНТАРЬ|")
+    print("• Дерево:", tree)
+    print("• Монеты:", coin)
+    
+    print(line)
+    
+    action_home = input("|Действие >>> ")
+    
+    if action_home == "Лес" or action_home == "лес":
+        
+        print("\n", line, sep="")
+        for i in range(5):
+            print(dot)
+        print(line)
+        
+        print("|КОМАНДЫ|")
+        print("• Рубить")
+        print("• Домой")
+        
+        loop_tree = True
+        while loop_tree:
+            action_tree = input("\n|Действие >>> ")
+            if action_tree == "Рубить" or action_tree == "рубить" or action_tree == "Рубит" or action_tree == "рубит":
+                while loop_tree:
+                    print("\n+ 1 дерево")
+                    tree +=1
+                    action_tree = input("\n|Действие >>> ")
+                    if action_tree == "Рубить" or action_tree == "рубить" or action_tree == "Рубит" or action_tree == "рубит":
+                        continue
+                    elif action_tree == "домой" or action_tree == "Домой" or action_tree == "Дом" or action_tree == "дом":
+                        loop_tree = False
+                    else:
+                        break
+            else:
+                break
+                
+    elif action_home == "Продать дерево" or action_home == "продать дерево":
+        if tree > 0:
+            loop_coin = True
+            while True:
+                print("+ " + str(tree * 5) + " монета (продано " + str(tree) + ")")
+                coin += tree
+                tree = 0
+                j = input()
+    
+    else:
+        continue
