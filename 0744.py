523bfa9eeb480cd065c6b998488569c46b4eda3e
Jones. <jonesmailhub@gmail.com>
2026-02-02 08:39:40 +0500

    Create 0744.py

---
+++ 0744.py
@@ -0,0 +1,15 @@
+x = 5
+
+print(10 <= 5 >= 4)
+print(abs(-7) > min(7, 3, 9, -3) > max(9, 3, 5, 3, 8.55))
+print(2 <= 5 >= 3)
+print(4 - 5 < 2 > -7)
+
+print(f"\n{not(6 < 5)}")
+print(not(True == True))
+print(not(False))
+
+print(f"\n{bool(1)}")
+print(bool(1 - 1))
+print(bool("word"))
+print(bool(-7))
