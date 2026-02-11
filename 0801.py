bfd00ba8728a8a45a13f3307429d26320f1a3f9b
Jones. <jonesmailhub@gmail.com>
2026-02-11 22:28:27 +0500

    Create 0801.py

---
+++ 0801.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+root.title("Программа")
+root.geometry("500x500")
+root.resizable(0, 0)
+
+root["bg"] = "purple"
+#root.config(bg = "green")
+
+root.mainloop()
+
+#root.iconbitmap("photo.png")
