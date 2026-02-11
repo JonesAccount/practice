bc033e8abb0d4c61df5c823037fa337fbfd6f97d
Jones. <jonesmailhub@gmail.com>
2026-02-11 22:28:42 +0500

    Create 0802.py

---
+++ 0802.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("программка")
+#root.iconbitmap("icon")
+root.geometry("450x500")
+root.resizable(0, 0)
+root.config(bg="yellow")
+
+root.mainloop()
