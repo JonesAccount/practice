adac400791d5a1b42b72cc76aa6fffbe33d701e6
Jones. <jonesmailhub@gmail.com>
2026-02-13 03:24:51 +0500

    Create 0829.py

---
+++ 0829.py
@@ -0,0 +1,36 @@
+from tkinter import *
+
+window = Tk()
+
+# ----------Page----------
+window.title("Program")
+window.config(bg="black")
+window.geometry("800x600")
+window.resizable(0, 0)
+
+
+# ----------Variable----------
+
+
+# ----------Function----------
+
+
+# ----------Frame----------
+
+
+# ----------Label----------
+
+
+# ----------Entry----------
+
+
+# ----------Text----------
+
+
+# ----------Button----------
+
+
+# ----------Show widget----------
+
+
+window.mainloop()
