42d7519f55ee3c2f63788592fce6341e65eae955
Jones. <jonesmailhub@gmail.com>
2026-02-16 10:15:37 +0500

    Create 0848.py

---
+++ 0848.py
@@ -0,0 +1,17 @@
+for i in range(0, 5):
+    print(i)
+    
+print()
+
+for i in range(5):
+    print(i)
+    
+print()
+    
+for i in range(0, 5, 1):
+    print(i)
+    
+print()
+
+for i in range(0, 5, 2):
+    print(i)
