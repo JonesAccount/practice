f1534be96992092e0bd2011c3e4ab7702da2fbd0
Jones. <jonesmailhub@gmail.com>
2026-02-20 22:06:11 +0500

    Create 0876.py

---
+++ 0876.py
@@ -0,0 +1,9 @@
+lst = ["яблоко", "банан", "вишня", "груша", "слива"]
+
+it = iter(lst)
+
+
+print(f" {next(it)}"); next(it)
+print(f" {next(it)}"); next(it)
+print(f" {next(it)}")
+print(" \nКонец списка")
