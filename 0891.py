a4e3b564473afe53f7699fc91206fe245e7f4f78
Jones. <jonesmailhub@gmail.com>
2026-02-20 22:10:17 +0500

    Create 0891.py

---
+++ 0891.py
@@ -0,0 +1,11 @@
+from random import randint
+
+N = [randint(-10, 10) for i in range(10)]
+M = ["Четное" if i % 2 == 0 else "Не четное" for i in N]
+D = dict()
+
+for i, v in enumerate(M):
+    D[i] = f"{N[i]} – {v}"
+    
+for key, value in D.items():
+    print(f" {key + 1}: {value}")
