e99d5aaf257c9c214261d9cbbc6bfca7b55f9578
Jones. <jonesmailhub@gmail.com>
2026-02-20 07:51:52 +0500

    Create 0868.py

---
+++ 0868.py
@@ -0,0 +1,9 @@
+d = dict(
+        one="Карбон",
+        two="Сецелий",
+        three="Фаприон",
+        four="Юнита",
+        five="Чигур")
+        
+for i, v in enumerate(d):
+    print(f" {i + 1} – {v} – ", end=""); print(d[v])
