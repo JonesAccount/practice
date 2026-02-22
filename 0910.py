30cbb498ad67c27e9af0b5fb3fab7811364cbd11
Jones. <jonesmailhub@gmail.com>
2026-02-22 20:59:54 +0500

    Create 0910.py

---
+++ 0910.py
@@ -0,0 +1,12 @@
+from time import sleep as sp
+import sys
+
+print("|o----|", end="\r", flush=True)
+sp(1)
+print("|-o---|", end="\r", flush=True)
+sp(1)
+print("|--o--|", end="\r", flush=True)
+sp(1)
+print("|---o-|", end="\r", flush=True)
+sp(1)
+print("|----o|", end="\r", flush=True)
