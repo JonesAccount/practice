9b30ee0bbef8403078ddaaedaf86df807cfb3723
Jones. <jonesmailhub@gmail.com>
2026-02-26 02:21:43 +0500

    Create 0928.py

---
+++ 0928.py
@@ -0,0 +1,13 @@
+from random import randint as ran
+
+number = ran(1, 3)
+
+while not True == False:
+    try:
+        player = int(input("Number: "
+        print("Win!") if player == nu        if player == number:
+            break
+    except EOFError:
+        print("Ввод не получен")
+                                     ~                                    ~
+~
