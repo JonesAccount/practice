bce986f863bf1c346b5c4f447fd13e330a210fcd
Jones. <jonesmailhub@gmail.com>
2026-03-03 12:32:04 +0500

    Create 0989.py

---
+++ 0989.py
@@ -0,0 +1,9 @@
+from rich import print
+from rich.console import Console
+
+print("[bold red on white]HELLO WORLD![/bold red on white]")
+
+console = Console()
+
+console.print("Text")
+console.print("[italic blue]Синий[/italic blue] и [bold purple]Фиолетовый[/bold purple]")
